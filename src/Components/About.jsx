c7301239dc8f83676b36bdbb4c68603418b5d3cf
divy-03 <127951881+divy-03@users.noreply.github.com>
2023-06-03 00:59:27 +0530

    update font color

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,7 +7,7 @@ function About(props) {
             <div className="inline info-title">
                 <h2>Why Choose Us</h2>
             </div>
-            <div className="inline info-body">
+            <div className={`inline info-body-${props.mode}`}>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quidem. Sit quos tempora rem minus
                 quibusdam veniam, eius laboriosam nulla, eos praesentium deserunt, ducimus eum eveniet. Tenetur sapiente
                 a in! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam quis reprehenderit
@@ -22,7 +22,7 @@ function About(props) {
                         Rust-Proof
                     </h2>
                 </div>
-                <div className="box-desc">
+                <div className={`box-desc-${props.mode}`}>
                     <p className="inline">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id tempora, cumque quibusdam nihil
                         beatae unde!
@@ -38,7 +38,7 @@ function About(props) {
                         Best-Quality
                     </h2>
                 </div>
-                <div className="box-desc">
+                <div className={`box-desc-${props.mode}`}>
                     <p className="inline">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id tempora, cumque quibusdam nihil
                         beatae unde!
@@ -54,7 +54,7 @@ function About(props) {
                         24*7 Services
                     </h2>
                 </div>
-                <div className="box-desc">
+                <div className={`box-desc-${props.mode}`}>
                     <p className="inline">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id tempora, cumque quibusdam nihil
                         beatae unde!
@@ -70,7 +70,7 @@ function About(props) {
                         New Designs
                     </h2>
                 </div>
-                <div className="box-desc">
+                <div className={`box-desc-${props.mode}`}>
                     <p className="inline">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id tempora, cumque quibusdam nihil
                         beatae unde!
